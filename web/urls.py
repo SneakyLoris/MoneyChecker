8401304fd0877c9d02b5c35ea708f712c9ab2fe9
Admin <tankn.qaz@gmail.com>
2025-03-05 17:32:51 +0300

    + registration

--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-from web.views import main_view
+from web.views import main_view, registration_view
 
 urlpatterns = [
     path('', main_view),
+    path('registration', registration_view),
 ]
--- web/views.py
+++ web/views.py
@@ -1,6 +1,31 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 
-# Create your views here.
+from web.forms import RegistrationForm
+
+User = get_user_model()
 
 def main_view(request):
     return render(request, 'web\main_view.html')
+
+
+def registration_view(request):
+    form = RegistrationForm()
+    is_success = False
+    if request.method == 'POST':
+        form = RegistrationForm(data=request.POST)
+        if form.is_valid():
+            user = User(
+                username=form.cleaned_data['username'],
+                email=form.cleaned_data['email'])
+            user.set_password(form.cleaned_data["password"])
+            user.save()
+            is_success = True
+
+            print(form.cleaned_data)
+
+
+    return render(request, r'web\registration_view.html', {
+        "form" : form,
+        "is_success" : is_success
+    })
